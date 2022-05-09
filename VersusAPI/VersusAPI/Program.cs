af13e959e0f348ce544c6b6bc696a644f3555e4d
Youcef k <youssefkaddour@hotmail.com>
2022-05-09 10:48:28 +0200

    Reglage des soucis lié au mauvais packagesNugget installé

--- VersusAPI/VersusAPI/Program.cs
+++ VersusAPI/VersusAPI/Program.cs
@@ -6,7 +6,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<DataContext>(options => {
+builder.Services.AddDbContext<DataContext>(options => 
+{
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle

d72295ff1238461ea3ef812de7ce5c824a607890
Youcef k <youssefkaddour@hotmail.com>
2022-05-09 10:28:23 +0200

    Ajout du repertoire DATA et du DataContext

--- VersusAPI/VersusAPI/Data/DataContext.cs
+++ VersusAPI/VersusAPI/Data/DataContext.cs
@@ -1,8 +1,13 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using VersusAPI.Models;
 
 namespace VersusAPI.Data
 {
     public class DataContext : DbContext
     {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Equipe> Equipes { get; set; }
+
     }
 }
